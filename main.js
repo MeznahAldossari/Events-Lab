f2b4bef7050bf84ad3641a24822485e5fcce1cf9
MeznahAldossari <MeznahAldossari@hotmail.com>
2024-05-27 17:19:27 +0300

    v1

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
 
 btn1.addEventListener("click", () => {
   text.innerText = "Welcome to JS BootCamp";
@@ -29,4 +30,19 @@ btn4.addEventListener("click", () => {
 
 btn5.addEventListener("click", () => {
   text.style.backgroundColor = "yellow";
+});
+
+btn6.addEventListener("click", () => {
+  let img = document.getElementById("image");
+  if (!img) {
+    let image = document.createElement("img");
+    let body = document.getElementById("body");
+    image.setAttribute("id", "image");
+    image.src = "download.png";
+    image.style.height = "20vh";
+    image.style.width = "20vh";
+    body.appendChild(image);
+  } else {
+    img.parentNode.removeChild(img);
+  }
 });
